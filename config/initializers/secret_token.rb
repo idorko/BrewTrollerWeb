4fadf4f300d4024346400d72f2d5278a8e08f17d
Ian Dorko <idorko@gmail.com>
2012-04-02 09:48:07 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewtroller::Application.config.secret_token = '44322f2f4f5912a1002cbf3c97db15a7b4223e65649e4f34533eb6db2d050f9449e5284c989f53b0ba725e95b4dc2db4f3164f7a155d14116c64c6ddf010ea0b'
